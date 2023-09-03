fe8da8615574b7ed86d8d7a8a03e1aa376b4cbd9
Nelson Chinedu <nelson.chinedu@prembly.com>
2023-09-03 14:03:37 +0100

    update path

--- example/src/app/page.tsx
+++ example/src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import Image from "next/image";
-import useIdentityPayKYC from "../../../src/index";
-import styles from "./page.module.css";
+import useIdentityPayKYC from "../../../index";
 
 export default function Home() {
   const config: any = {
@@ -10,18 +8,19 @@ export default function Home() {
     email: "kayode@myidentitypass.com",
     merchant_key: "",
     user_ref: "a unique ref for your user",
-    is_test: false, //set this to through for a test
-    config_id: "<configuration ID retrieve from your widget settings", //optional
+    is_test: false, //set this to true for a test
+    config_id: "<configuration ID retrieve from your widget settings>", //optional
     callback: (response: any) => console.log(response),
   };
 
   const verifyWithIdentity = useIdentityPayKYC(config);
 
   const handleTrigger = () => {
-    console.log(verifyWithIdentity(), "<>");
+    console.log(verifyWithIdentity());
   };
+
   return (
-    <main className={styles.main}>
+    <main>
       <button onClick={handleTrigger}>Click</button>
     </main>
   );
